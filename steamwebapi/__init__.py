dc16538ebe985cc7ea170f660169ebc2366efbf2
Shawn Silva <shawn.l.silva@gmail.com>
2016-11-02 14:21:55 -0400

    bumping dev version

--- steamwebapi/__init__.py
+++ steamwebapi/__init__.py
@@ -17,5 +17,5 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-__version__ = "0.1.3"
+__version__ = "0.1.4.dev1"
 __author__ = "Shawn Silva"
